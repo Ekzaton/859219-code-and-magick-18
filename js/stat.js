14c092b02599b2eddfb55ab7d081ff2edd765f7c
Ekzaton <ekzaton@gmail.com>
2019-09-22 23:02:57 +0500

    Правки для окна 2

--- js/stat.js
+++ js/stat.js
@@ -1,98 +1,82 @@
 'use strict';
 
-var cloud = {
-  width: 420,
-  height: 270,
-  x: 100,
-  y: 10,
-  gap: 10
+var Cloud = {
+  WIDTH: 420,
+  HEIGHT: 270,
+  X: 100,
+  Y: 10,
+  GAP: 10
 };
 
-var bar = {
-  width: 40,
-  height: 150,
-  gap: 50
+var Bar = {
+  WIDTH: 40,
+  HEIGHT: 150,
+  GAP: 50
 };
 
-var colors = {
-  cloudColor: '#fff',
-  cloudShadowColor: 'rgba(0, 0, 0, 0.7)',
-  fontColor: '#000',
-  yourColor: 'rgba(255, 0, 0, 1)'
+var Colors = {
+  WHITE: '#fff',
+  GRAY: 'rgba(0, 0, 0, 0.7)',
+  BLACK: '#000',
+  RED: 'rgba(255, 0, 0, 1)'
 };
 
-var hsl = {
-  hue: 240,
-  minSaturation: 20,
-  maxSaturation: 90,
-  lightness: '50%'
+var Hsl = {
+  HUE: 240,
+  MIN_SATURATION: 20,
+  MAX_SATURATION: 90,
+  LIGHTNESS: '50%'
 };
 
+//Отрисовка облака
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, Cloud.WIDTH, Cloud.HEIGHT);
+};
+
+//Отрисовка текста заголовка
+var renderTitle = function (ctx, text, x, y) {
+  ctx.fillStyle = Colors.BLACK;
+  ctx.textBaseline = 'hanging';
+
+  var lines = text.split("\n");
+
+  for (var i = 0; i < lines.length; i++) {
+ 		ctx.fillText(lines[i], x, (i + 1) * y);
+	}
+};
+
+//Отрисовка текста легенды
+var renderLegend = function (ctx, text, x, y) {
+  ctx.fillStyle = Colors.BLACK;
+  ctx.textBaseline = 'alphabetic';
+  ctx.fillText(text, x, y);
+};
+
+//Генерация заливики для столбцов статистики
 var getColor = function (hue, minSaturation, maxSaturation, lightness) {
   var saturation = Math.floor(Math.random() * (maxSaturation - minSaturation) + minSaturation) + '%';
   return ('hsl(' + hue + ', ' + saturation + ', ' + lightness + ')');
 };
 
-var getMaxElement = function (arr) {
-  var maxElement = arr[0];
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] > maxElement) {
-      maxElement = arr[i];
-    }
-  }
-  return maxElement;
-};
-
-var renderCloud = function (ctx, x, y, color) {
-  ctx.fillStyle = color;
-  ctx.fillRect(x, y, cloud.width, cloud.height);
-};
-
-window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx, cloud.x + cloud.gap, cloud.y + cloud.gap, colors.cloudShadowColor);
-  renderCloud(ctx, cloud.x, cloud.y, colors.cloudColor);
-
-  var textGap = cloud.x + 2 * cloud.gap;
-
-  ctx.fillStyle = colors.fontColor;
-  ctx.textBaseline = 'hanging';
-  ctx.fillText(
-      'Ура вы победили!',
-      textGap,
-      cloud.y + 2 * cloud.gap
-  );
-  ctx.fillText(
-      'Список результатов:',
-      textGap,
-      cloud.y + 4 * cloud.gap
-  );
-
-  ctx.textBaseline = 'alphabetic';
+//Отрисовка столбцов статистики
+var renderBar = function (ctx, names, times) {
   for (var i = 0; i < names.length; i++) {
-    var maxTime = getMaxElement(times);
-    var barHeight = (bar.height * times[i]) / maxTime;
-    var barGap = cloud.x + bar.gap * (i + 1) + bar.width * i;
+    var maxTime = Math.max.apply(null, times);
+    var barHeight = (Bar.HEIGHT * times[i]) / maxTime;
+    var barGap = Cloud.X + Bar.GAP * (i + 1) + Bar.WIDTH * i;
 
-    ctx.fillStyle = colors.fontColor;
-    ctx.fillText(
-        Math.floor(times[i]),
-        barGap,
-        cloud.height - cloud.gap * 3 - barHeight
-    );
-
-    ctx.fillStyle = (names[i] === 'Вы') ? colors.yourColor : getColor(hsl.hue, hsl.minSaturation, hsl.maxSaturation, hsl.lightness);
-    ctx.fillRect(
-        barGap,
-        cloud.height - cloud.gap * 2 - barHeight,
-        bar.width,
-        barHeight
-    );
-
-    ctx.fillStyle = colors.fontColor;
-    ctx.fillText(
-        names[i],
-        barGap,
-        cloud.height
-    );
+    renderLegend(ctx, Math.floor(times[i]), barGap, Cloud.HEIGHT - Cloud.GAP * 3 - barHeight);
+    ctx.fillStyle = (names[i] === 'Вы') ? Colors.RED : getColor(Hsl.HUE, Hsl.MIN_SATURATION, Hsl.MAX_SATURATION, Hsl.LIGHTNESS);
+    ctx.fillRect(barGap, Cloud.HEIGHT - Cloud.GAP * 2 - barHeight, Bar.WIDTH, barHeight);
+    renderLegend(ctx, names[i], barGap, Cloud.HEIGHT);
   }
+};
+
+//Отрисовка статистики
+window.renderStatistics = function (ctx, names, times) {
+  renderCloud(ctx, Cloud.X + Cloud.GAP, Cloud.Y + Cloud.GAP, Colors.GRAY);
+  renderCloud(ctx, Cloud.X, Cloud.Y, Colors.WHITE);
+  renderTitle(ctx, 'Ура вы победили!\nСписок результатов:', Cloud.X + 2 * Cloud.GAP, Cloud.Y + 1.5 * Cloud.GAP);
+  renderBar(ctx, names, times);
 };
