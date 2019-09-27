e3f89e2243ad113897c017e9f50ae9f9e8453c67
Ekzaton <ekzaton@gmail.com>
2019-09-28 00:07:31 +0500

    Правик для списка 4

--- js/setup.js
+++ js/setup.js
@@ -1,50 +1,53 @@
 'use strict';
 
-var setupPopup = document.querySelector('.setup');
-setupPopup.classList.remove('hidden');
-
-var setupSimilarList = setupPopup.querySelector('.setup-similar-list');
-var setupSimilarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-
-var WIZARDS = {
+// Константы
+var WIZARD = {
   name: {
-    firstName: ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'],
-    lastName: ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг']
+    first: ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'],
+    last: ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг']
   },
   coatColor: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
   eyesColor: ['black', 'red', 'blue', 'yellow', 'green']
 };
 var NUMBER_OF_WIZARDS = 4;
 
-// Генерация случайного элемента массива
-var getRandomItem = function (min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+// Переменные для DOM
+var setupPopup = document.querySelector('.setup');
+var setupSimilarList = setupPopup.querySelector('.setup-similar-list');
+var setupSimilarItem = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
+// Генерация индекса случайного элемента (minIndex - минимальный индекс, maxIndex - максимльный индекс)
+var getRandomIndex = function (minIndex, maxIndex) {
+  return Math.floor(Math.random() * (maxIndex - minIndex + 1)) + minIndex;
 };
 
-// Генерация массива данных для персонажей
+// Генерация объектов для массива данных о персонажах (number - количество персонажей)
 var getWizardData = function (number) {
   var wizardData = [];
 
   for (var i = 0; i < number; i++) {
-    if (getRandomItem(0, 1) === 0) {
-      var fullName = WIZARDS.name.firstName[getRandomItem(0, WIZARDS.name.firstName.length - 1)] + ' ' + WIZARDS.name.lastName[getRandomItem(0, WIZARDS.name.lastName.length - 1)];
-    } else {
-      fullName = WIZARDS.name.lastName[getRandomItem(0, WIZARDS.name.lastName.length - 1)] + ' ' + WIZARDS.name.firstName[getRandomItem(0, WIZARDS.name.firstName.length - 1)];
-    }
+    var firstName = WIZARD.name.first[getRandomIndex(0, WIZARD.name.first.length - 1)];
+    var lastName = WIZARD.name.last[getRandomIndex(0, WIZARD.name.last.length - 1)];
+    var fullName = firstName + ' ' + lastName;
 
-    wizardData[i] = {
+    var coatColor = WIZARD.coatColor[getRandomIndex(0, WIZARD.coatColor.length - 1)];
+    var eyesColor = WIZARD.eyesColor[getRandomIndex(0, WIZARD.eyesColor.length - 1)];
+
+    var newWizard = {
       name: fullName,
-      coatColor: WIZARDS.coatColor[getRandomItem(0, WIZARDS.coatColor.length - 1)],
-      eyesColor: WIZARDS.eyesColor[getRandomItem(0, WIZARDS.eyesColor.length - 1)]
+      coatColor: coatColor,
+      eyesColor: eyesColor
     };
+
+    wizardData.push(newWizard);
   }
 
   return wizardData;
 };
 
-// Создание персонажа
+// Создание персонажа (wizard - персонаж)
 var createWizard = function (wizard) {
-  var setupWizard = setupSimilarWizardTemplate.cloneNode(true);
+  var setupWizard = setupSimilarItem.cloneNode(true);
 
   setupWizard.querySelector('.setup-similar-label').textContent = wizard.name;
   setupWizard.querySelector('.wizard-coat').style.fill = wizard.coatColor;
@@ -53,7 +56,7 @@ var createWizard = function (wizard) {
   return setupWizard;
 };
 
-// Создание списка персонажей
+// Создание списка персонажей (wizardData - данные о персонаже)
 var createWizardsList = function (wizardData) {
   var setupWizardsList = document.createDocumentFragment();
 
@@ -62,7 +65,13 @@ var createWizardsList = function (wizardData) {
   }
 
   setupSimilarList.appendChild(setupWizardsList);
-  setupPopup.querySelector('.setup-similar').classList.remove('hidden');
 };
 
-createWizardsList(getWizardData(NUMBER_OF_WIZARDS));
+// Отрисовка окна
+var renderPopup = function (items) {
+  setupPopup.classList.remove('hidden');
+  createWizardsList(getWizardData(items));
+  setupPopup.querySelector('.setup-similar').classList.remove('hidden');
+}
+
+renderPopup(NUMBER_OF_WIZARDS);
